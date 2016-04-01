53d31fa94019bcbe65243fae5ac3d4c6fa9fc281
lithTech <lithtech@gmail.com>
2016-04-01 16:10:46 +0400

    - Refactor

--- app/src/main/java/lt/ru/lexio/ui/training/WordTranslationTrainingFragment.java
+++ app/src/main/java/lt/ru/lexio/ui/training/WordTranslationTrainingFragment.java
@@ -1,9 +1,7 @@
 package lt.ru.lexio.ui.training;
 
 import android.animation.Animator;
-import android.animation.ArgbEvaluator;
-import android.animation.ValueAnimator;
-import android.graphics.drawable.ColorDrawable;
+import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,7 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,6 +21,7 @@ import java.util.Random;
 
 import lt.ru.lexio.R;
 import lt.ru.lexio.db.Word;
+import lt.ru.lexio.db.WordStatistic;
 import lt.ru.lexio.util.ColorAnimateHelper;
 
 /**
@@ -33,17 +31,20 @@ public class WordTranslationTrainingFragment extends TrainingFragmentBase implem
 
     Word currentWord = null;
     long currentSessionId = 0;
-    int questionNum = 0;
-    List<Word> sessionWords = null;
-    List<String> sessionAnswers = null;
-    Random random = new Random(System.nanoTime());
+    protected int currentQuestionNum = 0;
+    protected List<Word> sessionWords = null;
+    protected List<String> sessionAnswers = null;
+    protected Random random = new Random(System.nanoTime());
 
     Animation aniCloseLastQuestion;
     Animation aniStartNewQuestion;
 
     Button[] bAnsArray;
+    private Button bDontKnow;
 
-    View parentLayout = null;
+    private View trainingPageContainer = null;
+    private View endPageContainer = null;
+
     private Drawable initialButtonBkg;
 
     @Override
@@ -81,16 +82,18 @@ public class WordTranslationTrainingFragment extends TrainingFragmentBase implem
         if (!isCorrect) {
             ColorAnimateHelper.animateBetweenColors(clickedButton,
                     android.R.drawable.btn_default,
-                    getResources().getColor(R.color.colorMandatory), 2000, onEndAnimation);
+                    getResources().getColor(R.color.colorMandatory),
+                    getResources().getInteger(R.integer.animation_incorrect_answer), onEndAnimation);
             onEndAnimation = null;      //animation of wrong answer is longer, so put onEndAnimation event there
         }
         ColorAnimateHelper.animateBetweenColors(correct,
                 android.R.drawable.btn_default,
-                getResources().getColor(R.color.correctAnswer), 400, onEndAnimation);
+                getResources().getColor(R.color.correctAnswer),
+                getResources().getInteger(R.integer.animation_correct_answer), onEndAnimation);
     }
 
     protected void startTraining() {
-        questionNum = 0;
+        currentQuestionNum = 0;
         trainingWordBuilder.dictId = getCurrentDictionary().id;
         sessionWords = trainingWordBuilder.build(15, TrainingWordMethod.UNTRAINING_WORDS,
                 TrainingType.WORD_TRANS);
@@ -123,32 +126,42 @@ public class WordTranslationTrainingFragment extends TrainingFragmentBase implem
     }
 
     private void nextQuestion(boolean animateQuestionExit) {
-        if (questionNum >= sessionWords.size()) {
-            exitTraining();
-            return;
-        }
         //exit from old question is not yet animated
         if (animateQuestionExit) {
-            parentLayout.startAnimation(aniCloseLastQuestion);
+            trainingPageContainer.startAnimation(aniCloseLastQuestion);
         }
         //animation on end question is ended, present new question
         else {
-            currentWord = sessionWords.get(questionNum);
+            if (currentQuestionNum >= sessionWords.size()) {
+                exitTraining();
+                return;
+            }
+            currentWord = sessionWords.get(currentQuestionNum);
             String correctAnswer = currentWord.getTranslation();
             List<String> answers = buildQuestionAnswers(correctAnswer);
-            int correctNum = answers.indexOf(correctAnswer) + 1;
+            int correctNum = answers.indexOf(correctAnswer);
 
             setQuestion(currentWord.getTitle(), currentWord.getContext(), answers,
                     correctNum);
 
-            questionNum++;
+            currentQuestionNum++;
 
-            parentLayout.startAnimation(aniStartNewQuestion);
+            toggleEnabledControls(true);
+            trainingPageContainer.startAnimation(aniStartNewQuestion);
         }
     }
 
     private void exitTraining() {
+        trainingPageContainer.setVisibility(View.GONE);
+        trainingPageContainer.invalidate();
 
+        endPageContainer.setVisibility(View.VISIBLE);
+
+        Cursor statCur = wordStatisticDAO.getTrainStatisticsBySession(currentSessionId);
+        while (statCur.moveToNext()) {
+            WordStatistic wordStatistic = wordStatisticDAO.readRow(statCur);
+
+        }
     }
 
     protected Button findCorrectAnswerButton() {
@@ -173,7 +186,7 @@ public class WordTranslationTrainingFragment extends TrainingFragmentBase implem
         for (int i = 0; i < bAnsArray.length; i++) {
             Button ans = bAnsArray[i];
             ans.setTag(correctNumIndex == i);
-            ans.setTag(answers.get(i));
+            ans.setText(answers.get(i));
         }
     }
 
@@ -182,7 +195,8 @@ public class WordTranslationTrainingFragment extends TrainingFragmentBase implem
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = super.onCreateView(inflater, container, savedInstanceState);
 
-        parentLayout = view.findViewById(R.id.layout_word_train);
+        trainingPageContainer = view.findViewById(R.id.layout_word_train);
+        endPageContainer = view.findViewById(R.id.layout_train_end_page);
 
         bAnsArray = new Button[4];
 
@@ -190,6 +204,8 @@ public class WordTranslationTrainingFragment extends TrainingFragmentBase implem
         bAnsArray[1] = (Button) view.findViewById(R.id.bWordTransAnswer2);
         bAnsArray[2] = (Button) view.findViewById(R.id.bWordTransAnswer3);
         bAnsArray[3] = (Button) view.findViewById(R.id.bWordTransAnswer4);
+        bDontKnow = (Button) view.findViewById(R.id.bWordTransDontKnow);
+
 
         initialButtonBkg = bAnsArray[0].getBackground();
 
@@ -217,13 +233,21 @@ public class WordTranslationTrainingFragment extends TrainingFragmentBase implem
         for (Button ans : bAnsArray) {
             ans.setOnClickListener(this);
         }
-
+        bDontKnow.setTag(false);
+        bDontKnow.setOnClickListener(this);
 
         return view;
     }
 
+    protected void toggleEnabledControls(boolean enabled) {
+        for (Button ans : bAnsArray) {
+            ans.setEnabled(enabled);
+        }
+    }
+
     @Override
     public void onClick(View v) {
+        toggleEnabledControls(false);
         Button bAns = (Button) v;
         boolean correct = (boolean) bAns.getTag();
         onAnswer(correct, bAns);
