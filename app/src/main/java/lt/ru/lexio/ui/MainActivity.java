e0d4b69cb47f88ceee9996d18107dacd0668d7a6
lithTech <lithTech@gmail.com>
2016-12-25 18:03:51 +0300

    - Back button toggle navibar

--- app/src/main/java/lt/ru/lexio/ui/MainActivity.java
+++ app/src/main/java/lt/ru/lexio/ui/MainActivity.java
@@ -129,7 +129,7 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            drawer.openDrawer(GravityCompat.START);
         }
     }
 
